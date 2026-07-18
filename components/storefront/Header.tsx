3216fbfc6b93e7f2129aa5457c38b70caa1ce70e
Arsenio riszaid <arsenioriszaid@gmail.com>
2026-07-18 18:04:08 +0700

    Add files via upload

--- components/storefront/Header.tsx
+++ components/storefront/Header.tsx
@@ -38,7 +38,7 @@ const mobileNavItemVariants = {
   visible: {
     opacity: 1,
     x: 0,
-    transition: { duration: 0.35, ease: [0.4, 0, 0.2, 1] },
+    transition: { duration: 0.35, ease: [0.4, 0, 0.2, 1] as const },
   },
 };
 
